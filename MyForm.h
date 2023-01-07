c51d9b8ed8ecdfb232aa51d8d8eac606ab02851f
Raushan Kumar <raushankumargalaxy407@gmail.com>
2023-01-07 07:08:08 +0530

    Add project files.

---
+++ MyForm.h
@@ -0,0 +1,380 @@
+﻿#pragma once
+
+namespace ModernLoginForm {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			textBox1->Focus();
+			pnlTerms->Hide();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Panel^ panel1;
+
+
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Panel^ pnlTerms;
+
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Button^ button3;
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->pnlTerms = (gcnew System::Windows::Forms::Panel());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->pnlTerms->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 28.75F));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(61, 90);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(173, 65);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Sign in";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Black;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 8.75F));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(68, 223);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(75, 20);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Username";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Black;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Segoe UI", 8.75F));
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(68, 316);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(70, 20);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Password";
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::Color::Black;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Verdana", 10.75F));
+			this->textBox1->ForeColor = System::Drawing::Color::White;
+			this->textBox1->Location = System::Drawing::Point(72, 252);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(316, 22);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::textBox1_KeyDown);
+			// 
+			// panel1
+			// 
+			this->panel1->Location = System::Drawing::Point(72, 275);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(316, 2);
+			this->panel1->TabIndex = 2;
+			// 
+			// panel2
+			// 
+			this->panel2->Location = System::Drawing::Point(72, 370);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(316, 2);
+			this->panel2->TabIndex = 2;
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->BackColor = System::Drawing::Color::Black;
+			this->checkBox1->Font = (gcnew System::Drawing::Font(L"Verdana", 8.75F));
+			this->checkBox1->Location = System::Drawing::Point(73, 398);
+			this->checkBox1->Margin = System::Windows::Forms::Padding(0);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(132, 22);
+			this->checkBox1->TabIndex = 3;
+			this->checkBox1->TabStop = false;
+			this->checkBox1->Text = L"I agree to the";
+			this->checkBox1->UseVisualStyleBackColor = false;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox1_CheckedChanged);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Black;
+			this->label4->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Segoe UI", 8.75F));
+			this->label4->ForeColor = System::Drawing::Color::LightCoral;
+			this->label4->Location = System::Drawing::Point(200, 398);
+			this->label4->Margin = System::Windows::Forms::Padding(0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(152, 20);
+			this->label4->TabIndex = 0;
+			this->label4->Text = L"Terms and Conditions";
+			this->label4->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::IndianRed;
+			this->button1->Enabled = false;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Verdana", 10.75F));
+			this->button1->Location = System::Drawing::Point(72, 466);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(118, 42);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Sign in";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Black;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Verdana", 10.75F));
+			this->button2->ForeColor = System::Drawing::Color::IndianRed;
+			this->button2->Location = System::Drawing::Point(210, 466);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(118, 42);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Exit";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->BackColor = System::Drawing::Color::Black;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Verdana", 10.75F));
+			this->textBox2->ForeColor = System::Drawing::Color::White;
+			this->textBox2->Location = System::Drawing::Point(72, 344);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(316, 22);
+			this->textBox2->TabIndex = 2;
+			this->textBox2->UseSystemPasswordChar = true;
+			this->textBox2->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::textBox2_KeyDown);
+			// 
+			// pnlTerms
+			// 
+			this->pnlTerms->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->pnlTerms->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pnlTerms.BackgroundImage")));
+			this->pnlTerms->Controls->Add(this->button3);
+			this->pnlTerms->Controls->Add(this->textBox3);
+			this->pnlTerms->Controls->Add(this->label5);
+			this->pnlTerms->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pnlTerms->Location = System::Drawing::Point(0, 0);
+			this->pnlTerms->Name = L"pnlTerms";
+			this->pnlTerms->Size = System::Drawing::Size(1137, 701);
+			this->pnlTerms->TabIndex = 5;
+			this->pnlTerms->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseDown);
+			this->pnlTerms->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseMove);
+			this->pnlTerms->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseUp);
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::IndianRed;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Verdana", 10.75F));
+			this->button3->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->button3->Location = System::Drawing::Point(979, 398);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(120, 40);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"OK";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// textBox3
+			// 
+			this->textBox3->BackColor = System::Drawing::Color::Black;
+			this->textBox3->ForeColor = System::Drawing::Color::IndianRed;
+			this->textBox3->Location = System::Drawing::Point(99, 112);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(1013, 142);
+			this->textBox3->TabIndex = 1;
+			this->textBox3->Text = resources->GetString(L"textBox3.Text");
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(93, 53);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(307, 32);
+			this->label5->TabIndex = 0;
+			this->label5->Text = L"Terms and Conditions";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1137, 701);
+			this->Controls->Add(this->pnlTerms);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Font = (gcnew System::Drawing::Font(L"Verdana", 15.75F));
+			this->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseDown);
+			this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseMove);
+			this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseUp);
+			this->pnlTerms->ResumeLayout(false);
+			this->pnlTerms->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		 }
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		
+	}
+	private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+		if (checkBox1->Checked)
+			button1->Enabled = true;
+		else button1->Enabled = false;
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	Application::Exit();
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (textBox1->Text == "admin")
+		if (textBox2->Text == "root") {
+			MessageBox::Show("Access Granted❤️", "Sign in successfull⭐", MessageBoxButtons::OK, MessageBoxIcon::Information);
+			Application::Exit();
+		}
+			
+		else
+		{
+			MessageBox::Show("Incorrect Password", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		}
+	else
+	{
+		MessageBox::Show("Invalid User", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+}
+
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+	pnlTerms->Show();
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	pnlTerms->Hide();
+}
+
+	   //Dragging the form.
+	   bool dragging;
+	   Point offset;
+
+private: System::Void MyForm_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		// Enable dragging and get mouse position.
+	dragging = true;
+	offset.X = e->X;
+	offset.Y = e->Y;
+}
+private: System::Void MyForm_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		// Whether we are able to move.
+	if (dragging) {
+		Point currentScreenpositon = PointToScreen(Point(e->X, e->Y));
+		Location = Point(currentScreenpositon.X - offset.X, currentScreenpositon.Y - offset.Y); 
+		}
+}
+private: System::Void MyForm_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+	dragging = false;
+}
+	   //admin
+private: System::Void textBox1_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+	if (e->KeyValue == (int)Keys::Enter)
+		textBox2->Focus();
+}
+	   //root
+private: System::Void textBox2_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+	if (e->KeyValue == (int)Keys::Enter)
+		button1->PerformClick();
+}
+};
+}
